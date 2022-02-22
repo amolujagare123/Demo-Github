9a53ed28db6a32727bdaf0c3d20dbd2f465db050
amolu <amolujagare@gmail.com>
2022-02-22 08:13:07 +0530

    Initial commit

---
+++ src/Addition.java
@@ -0,0 +1,55 @@
+public class Addition {
+
+    int a;
+    int b;
+
+    void addition()
+    {
+        int c;
+        c = a+b;
+        System.out.println("Addition="+c);
+    }
+
+    void addition(int x,int y)
+    {
+        int c;
+        c = x+y;
+        System.out.println("Addition="+c);
+    }
+    void addition(double x,double y)
+    {
+        double c;
+        c = x+y;
+        System.out.println("Addition="+c);
+    }
+
+    void addition(String x,String y)
+    {
+        String c;
+        c = x+y;
+        System.out.println("Addition="+c);
+    }
+
+    void addition(int x,int y,int z)
+    {
+        int c;
+        c = x+y+z;
+        System.out.println("Addition="+c);
+    }
+
+    public static void main(String[] args) {
+
+        Addition ob = new Addition();
+        ob.a = 11;
+        ob.b =22;
+        ob.addition();
+
+        ob.addition(23,11);
+        ob.addition(20,30,40);
+
+        ob.addition(23.45,25.11);
+        ob.addition("Java+","Selenium");
+
+
+    }
+}
---
+++ src/CastingDemo.java
@@ -0,0 +1,13 @@
+public class CastingDemo {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 3;
+
+        double c = (double)  a/b;
+        // casting - when you want to store one type of value into another type
+        // we write that type into round bracket in from of value to be stored
+
+        System.out.println("c="+c);
+    }
+}
